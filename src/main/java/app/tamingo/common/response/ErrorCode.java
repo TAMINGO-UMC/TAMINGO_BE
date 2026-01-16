176149e6cc1d0ee01ad1c4c065f9d1d4089fa952
HeejuKo <khjg00gle96@gmail.com>
2026-01-17 01:18:43 +0900

    Feat: 약관 관련 에러 코드 추가

--- src/main/java/app/tamingo/common/response/ErrorCode.java
+++ src/main/java/app/tamingo/common/response/ErrorCode.java
@@ -9,6 +9,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode implements BaseCode {
 
+    // 약관
+    TERMS_NOT_FOUND(HttpStatus.NOT_FOUND, "TERMS-001", "해당 약관을 찾을 수 없습니다."),
+    REQUIRED_TERMS_NOT_AGREED(HttpStatus.BAD_REQUEST, "TERMS-002", "필수 약관에 동의하지 않았습니다."),
+    INVALID_TERMS_REQUEST(HttpStatus.BAD_REQUEST, "TERMS-003", "약관 요청이 올바르지 않습니다."),
+
     // 서버 에러
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON-001", "서버 오류가 발생했습니다."),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON-002", "요청 파라미터가 올바르지 않습니다."),
---
+++ src/main/java/app/tamingo/domain/terms/service/TermsService.java
@@ -0,0 +1,4 @@
+package app.tamingo.domain.terms.service;
+
+public class TermsService {
+}
