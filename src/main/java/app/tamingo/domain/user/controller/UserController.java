f0277f41bc3a4bf0448fc3d7c494b72947fdfc9f
HeejuKo <khjg00gle96@gmail.com>
2026-02-03 00:26:06 +0900

    [#100] Feature: 회원 탈퇴(Soft Delete) 구현

---
+++ src/main/java/app/tamingo/domain/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package app.tamingo.domain.user.controller;
+
+import app.tamingo.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> withdraw(@AuthenticationPrincipal Long userId) {
+        userService.withdraw(userId);
+        return ResponseEntity.noContent().build();
+    }
+}
