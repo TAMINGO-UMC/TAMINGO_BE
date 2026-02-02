c81ef3ec776b2506168fa83197c0a96a5e3fde18
HeejuKo <khjg00gle96@gmail.com>
2026-02-03 01:12:56 +0900

    [#100] Refactor: 회원 탈퇴 API 공통 응답 적용

--- src/main/java/app/tamingo/domain/user/controller/UserController.java
+++ src/main/java/app/tamingo/domain/user/controller/UserController.java
@@ -1,8 +1,9 @@
 package app.tamingo.domain.user.controller;
 
+import app.tamingo.common.response.ApiResponse;
+import app.tamingo.common.response.SuccessCode;
 import app.tamingo.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,8 @@ public class UserController {
     private final UserService userService;
 
     @DeleteMapping("/me")
-    public ResponseEntity<Void> withdraw(@AuthenticationPrincipal Long userId) {
+    public ApiResponse<Void> withdraw(@AuthenticationPrincipal Long userId) {
         userService.withdraw(userId);
-        return ResponseEntity.noContent().build();
+        return ApiResponse.onSuccess(null, SuccessCode.NO_CONTENT);
     }
 }
