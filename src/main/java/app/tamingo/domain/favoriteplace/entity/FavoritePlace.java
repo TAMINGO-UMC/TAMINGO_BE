358839a074402f5522e52898541ce3995f207ca4
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-02-03 21:14:57 +0900

    [#77] Chore: 충돌 수정

--- src/main/java/app/tamingo/domain/favoriteplace/entity/FavoritePlace.java
+++ src/main/java/app/tamingo/domain/favoriteplace/entity/FavoritePlace.java
@@ -16,7 +16,7 @@ import lombok.*;
                 @Index(name = "idx_favorite_place_user_id", columnList = "user_id")
         }
 )
-public class FavoritePlaceStandard extends BaseEntity {
+public class FavoritePlace extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,7 +42,7 @@ public class FavoritePlaceStandard extends BaseEntity {
     private Double longitude;
 
     @Builder
-    private FavoritePlaceStandard(User user, String name, String address, Double latitude, Double longitude) {
+    private FavoritePlace(User user, String name, String address, Double latitude, Double longitude) {
         this.user = user;
         this.name = name;
         this.address = address;
@@ -50,8 +50,8 @@ public class FavoritePlaceStandard extends BaseEntity {
         this.longitude = longitude;
     }
 
-    public static FavoritePlaceStandard of(User user, String name, String address, Double latitude, Double longitude) {
-        return FavoritePlaceStandard.builder()
+    public static FavoritePlace of(User user, String name, String address, Double latitude, Double longitude) {
+        return FavoritePlace.builder()
                 .user(user)
                 .name(name)
                 .address(address)
--- src/main/java/app/tamingo/domain/favoriteplace/service/FavoritePlaceService.java
+++ src/main/java/app/tamingo/domain/favoriteplace/service/FavoritePlaceService.java
@@ -4,9 +4,9 @@ import app.tamingo.common.exception.CustomException;
 import app.tamingo.common.response.ErrorCode;
 import app.tamingo.domain.favoriteplace.dto.FavoritePlaceRequest;
 import app.tamingo.domain.favoriteplace.dto.FavoritePlaceResponse;
-import app.tamingo.domain.favoriteplace.entity.FavoritePlaceStandard;
+import app.tamingo.domain.favoriteplace.entity.FavoritePlace;
 import app.tamingo.domain.favoriteplace.exception.FavoritePlaceErrorCode;
-import app.tamingo.domain.favoriteplace.repository.FavoritePlaceStandardRepository;
+import app.tamingo.domain.favoriteplace.repository.FavoritePlaceRepository;
 import app.tamingo.domain.user.entity.User;
 import app.tamingo.domain.user.exception.UserErrorCode;
 import app.tamingo.domain.user.repository.UserRepository;
@@ -22,7 +22,7 @@ import java.util.Set;
 @Service
 @Transactional(readOnly = true)
 public class FavoritePlaceService {
-    private final FavoritePlaceStandardRepository favoritePlaceRepository;
+    private final FavoritePlaceRepository favoritePlaceRepository;
     private final UserRepository userRepository;
 
     // 자주 가는 장소 등록
@@ -37,7 +37,7 @@ public class FavoritePlaceService {
             throw new CustomException(FavoritePlaceErrorCode.FAVORITE_PLACE_ALREADY_EXISTS);
         }
 
-        FavoritePlaceStandard favoritePlaceStandard =  FavoritePlaceStandard.of(
+        FavoritePlace favoritePlace =  FavoritePlace.of(
                 user,
                 request.name(),
                 request.address(),
@@ -45,7 +45,7 @@ public class FavoritePlaceService {
                 request.longitude()
                 );
 
-        return favoritePlaceRepository.save(favoritePlaceStandard).getId();
+        return favoritePlaceRepository.save(favoritePlace).getId();
     }
 
     // 특정 유저의 자주 가는 장소 목록 조회
@@ -61,19 +61,19 @@ public class FavoritePlaceService {
     // 자주 가는 장소 정보 수정 (더티 체킹 사용)
     @Transactional
     public void update(Long id, FavoritePlaceRequest.UpdateDto request) {
-        FavoritePlaceStandard favoritePlaceStandard = favoritePlaceRepository.findById(id)
+        FavoritePlace favoritePlace = favoritePlaceRepository.findById(id)
                 .orElseThrow(() -> new CustomException(FavoritePlaceErrorCode.FAVORITE_PLACE_NOT_FOUND));
 
         // 이미 등록된 주소나 장소로 변경하는지 확인 (자신은 제외)
         if (favoritePlaceRepository.existsForUpdate(
-                favoritePlaceStandard.getUser(),
+                favoritePlace.getUser(),
                 request.name(),
                 request.address(),
                 id)) {
             throw new CustomException(FavoritePlaceErrorCode.FAVORITE_PLACE_ALREADY_EXISTS);        }
 
         // 엔티티 변경시 트랜잭션 종료와 함께 자동 db 반영
-        favoritePlaceStandard.update(
+        favoritePlace.update(
                 request.name(),
                 request.address(),
                 request.latitude(),
@@ -84,10 +84,10 @@ public class FavoritePlaceService {
     // 자주 가는 장소 삭제
     @Transactional
     public void delete(Long id) {
-        FavoritePlaceStandard favoritePlaceStandard = favoritePlaceRepository.findById(id)
+        FavoritePlace favoritePlace = favoritePlaceRepository.findById(id)
                 .orElseThrow(() -> new CustomException(FavoritePlaceErrorCode.FAVORITE_PLACE_NOT_FOUND));
 
-        favoritePlaceRepository.delete(favoritePlaceStandard);
+        favoritePlaceRepository.delete(favoritePlace);
     }
 
     // 온보딩 초기 세팅: 자주 가는 장소 등록
@@ -122,7 +122,7 @@ public class FavoritePlaceService {
 
         for (FavoritePlaceRequest.SaveDto p : list) {
             favoritePlaceRepository.save(
-                    FavoritePlaceStandard.of(
+                    FavoritePlace.of(
                             user,
                             p.name(),
                             p.address(),
