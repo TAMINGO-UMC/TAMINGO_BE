9a617df8ab1d6ab07e4fa9faab0dd42e3e88465c
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-02-03 21:14:18 +0900

    [#77] Feat: tmap 연결 및 카카오맵 연결

--- src/main/java/app/tamingo/common/config/WebClientConfig.java
+++ src/main/java/app/tamingo/common/config/WebClientConfig.java
@@ -10,26 +10,48 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @Bean
-    WebClient gptWebClient(
-            @Value("${chatgpt.url}") String baseUrl,
-            @Value("${chatgpt.api-key}") String apiKey
-    ) {
-        return WebClient.builder()
-                .baseUrl(baseUrl)
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
-    @Bean(name = "kakaoWebClient")
-    WebClient kakaoWebClient(
-            @Value("${kakao.api.key}") String apiKey,
-            @Value("${kakao.api.url}") String baseUrl
-    ) {
-        return WebClient.builder()
-                .baseUrl(baseUrl) // [변경] 직접 입력 -> 변수 사용
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "KakaoAK " + apiKey)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
+        @Bean
+        WebClient gptWebClient(
+                        @Value("${chatgpt.url}") String baseUrl,
+                        @Value("${chatgpt.api-key}") String apiKey) {
+                return WebClient.builder()
+                                .baseUrl(baseUrl)
+                                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
+                                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                .build();
+        }
+
+        @Bean(name = "kakaoWebClient")
+        WebClient kakaoWebClient(
+                        @Value("${kakao.api.key}") String apiKey,
+                        @Value("${kakao.api.url}") String baseUrl) {
+                return WebClient.builder()
+                                .baseUrl(baseUrl) // [변경] 직접 입력 -> 변수 사용
+                                .defaultHeader(HttpHeaders.AUTHORIZATION, "KakaoAK " + apiKey)
+                                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                .build();
+        }
+
+        @Bean(name = "tmapWebClient")
+        WebClient tmapWebClient(
+                        @Value("${tmap.url}") String baseUrl,
+                        @Value("${tmap.app-key}") String appKey) {
+                return WebClient.builder()
+                                .baseUrl(baseUrl)
+                                .defaultHeader("appKey", appKey)
+                                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                .build();
+        }
+
+        @Bean(name = "kakaoMapGeoWebClient")
+        WebClient kakaoMapGeoClient(
+                        @Value("${kakao.api.key}") String apiKey,
+                        @Value("${kakao.api.url}") String baseUrl) {
+                return WebClient.builder()
+                        .baseUrl(baseUrl)
+                        .defaultHeader(HttpHeaders.AUTHORIZATION, "KakaoAK " + apiKey)
+                        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .build();
+        }
 }
