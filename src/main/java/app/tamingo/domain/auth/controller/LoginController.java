ee636e788c54d585f715807b89ac119399736779
HeejuKo <khjg00gle96@gmail.com>
2026-02-05 00:30:35 +0900

    [#135] Docs: auth Controller 설명 추가

--- src/main/java/app/tamingo/domain/auth/controller/LoginController.java
+++ src/main/java/app/tamingo/domain/auth/controller/LoginController.java
@@ -7,6 +7,8 @@ import app.tamingo.domain.auth.dto.login.LoginRequest;
 import app.tamingo.domain.auth.dto.login.LoginResponse;
 import app.tamingo.domain.auth.service.auth.KakaoLoginService;
 import app.tamingo.domain.auth.service.auth.LoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "이메일 로그인 및 카카오 로그인 API"
+)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
@@ -22,6 +27,10 @@ public class LoginController {
     private final LoginService loginService;
     private final KakaoLoginService kakaoLoginService;
 
+    @Operation(
+            summary = "이메일 로그인",
+            description = "이메일/비밀번호로 로그인하고 access/refresh 토큰을 발급합니다."
+    )
     @PostMapping("/login")
     public ApiResponse<LoginResponse> login(@RequestBody @Valid LoginRequest req) {
         return ApiResponse.onSuccess(
@@ -30,6 +39,10 @@ public class LoginController {
         );
     }
 
+    @Operation(
+            summary = "카카오 로그인",
+            description = "카카오 액세스 토큰으로 로그인하고 access/refresh 토큰을 발급합니다."
+    )
     @PostMapping("/login/kakao")
     public ApiResponse<LoginResponse> kakaoLogin(@RequestBody @Valid KakaoLoginRequest req) {
         return ApiResponse.onSuccess(
