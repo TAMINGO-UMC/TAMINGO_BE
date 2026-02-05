2537f37f739e13cd14960e3ba9dadbc580e6d5fc
baseballclub <abkimdoyo25@gmail.com>
2026-02-05 23:41:27 +0900

    [Chore] 에러 코드 파일명 통일화

--- src/main/java/app/tamingo/domain/useractivetime/exception/UserActiveTimeErrorCode.java
+++ src/main/java/app/tamingo/domain/useractivetime/exception/UserActiveTimeErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum UserActiveTimeError implements BaseCode {
+public enum UserActiveTimeErrorCode implements BaseCode {
 
     TIME_ORDER_INVALID(HttpStatus.BAD_REQUEST, "TIME-001", "종료 시간은 시작 시간보다 이후여야 합니다."),
     TIME_FORMAT_INVALID(HttpStatus.BAD_REQUEST, "TIME-002", "활동 시간 형식이 올바르지 않습니다. (HH:mm)");
