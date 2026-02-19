9f9858215022bbdd61cb1986e6ec2b8bb7b8dfd8
hyeonkangkimm <kkhk75@gmail.com>
2026-02-19 22:48:28 +0900

    Feat: dev 컨트롤러 swagger 추가

--- src/main/java/app/tamingo/dev/controller/MonthlyReportDevController.java
+++ src/main/java/app/tamingo/dev/controller/MonthlyReportDevController.java
@@ -3,13 +3,15 @@ package app.tamingo.dev.controller;
 import app.tamingo.common.response.ApiResponse;
 import app.tamingo.common.response.SuccessCode;
 import app.tamingo.domain.monthlyreport.batch.MonthlyReportBatchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
 
-
+@Tag(name = "개발용 월간리포트 스케줄러 API", description = "데모 시뮬레이션을 위한 스케줄러 실행")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/dev/reports")
@@ -18,6 +20,7 @@ public class MonthlyReportDevController {
     private final MonthlyReportBatchService monthlyReportBatchService;
 
     // 예: POST /api/dev/reports/monthly?yearMonth=2026-01
+    @Operation(summary = "스케줄러 실행", description = "전체 사용자 월간리포트 스케줄링을 합니다.")
     @PostMapping("/monthly")
     public ApiResponse<LocalDate> generateMonthly(@RequestParam("yearMonth") String yearMonth) {
         YearMonth ym = YearMonth.parse(yearMonth);
--- src/main/java/app/tamingo/dev/controller/WeeklyReportDevController.java
+++ src/main/java/app/tamingo/dev/controller/WeeklyReportDevController.java
@@ -3,6 +3,8 @@ package app.tamingo.dev.controller;
 import app.tamingo.common.response.ApiResponse;
 import app.tamingo.common.response.SuccessCode;
 import app.tamingo.domain.weeklyreport.batch.WeeklyReportBatchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 
+@Tag(name = "개발용 주간리포트 스케줄러 API", description = "데모 시뮬레이션을 위한 스케줄러 실행")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/dev/reports")
@@ -19,6 +22,7 @@ public class WeeklyReportDevController {
     private final WeeklyReportBatchService weeklyReportBatchService;
 
     // 예: POST /api/dev/reports/weekly?weekStartDate=2026-01-26
+    @Operation(summary = "스케줄러 실행", description = "전체 사용자 주간리포트 스케줄링을 합니다.")
     @PostMapping("/weekly")
     public ApiResponse<LocalDate> generateWeekly(
             @RequestParam("weekStartDate")
