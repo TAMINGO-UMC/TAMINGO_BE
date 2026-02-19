2ee555b5fa55e44190e78c2045026280f06798fa
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-02-19 22:10:47 +0900

    Chore: dev 경로 인증 거치지 않도록 수정

--- src/main/java/app/tamingo/common/config/SecurityConfig.java
+++ src/main/java/app/tamingo/common/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/api/terms/**",
+                                "/api/dev/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll()
